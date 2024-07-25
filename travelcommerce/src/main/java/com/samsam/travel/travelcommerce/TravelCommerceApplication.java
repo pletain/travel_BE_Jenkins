6e2faa961f1fb1c27cccd60222e3b801ef109659
KIM TAE HYEONG <thk12378905@gmail.com>
2024-07-25 18:25:55 +0900

    chore: 프로젝트 초기화

---
+++ travelcommerce/src/main/java/com/samsam/travel/travelcommerce/TravelCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.samsam.travel.travelcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelCommerceApplication.class, args);
+	}
+
+}
---
+++ travelcommerce/src/test/java/com/samsam/travel/travelcommerce/TravelCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsam.travel.travelcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
