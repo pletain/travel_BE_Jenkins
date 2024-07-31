07e7d263b1597121ef5cc6dc38805d95e6dbb718
KIM TAE HYEONG <thk12378905@gmail.com>
2024-07-31 17:49:01 +0900

    feat: Review 엔티티 클래스 생성

---
+++ src/main/java/com/samsam/travel/travelcommerce/dao/Review.java
@@ -0,0 +1,38 @@
+package com.samsam.travel.travelcommerce.dao;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @Column(name = "review_id")
+    private String reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Orders orders;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(name = "comment", nullable = false)
+    private String comment;
+
+    @Column(name = "rating", nullable = false)
+    private float rating;
+
+    @Column(name = "regist_date", nullable = false)
+    private LocalDateTime registDate;
+
+}
