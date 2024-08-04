0f6c77abb45d7d9536e390706a5257868986f025
ChoiSeungWoo98 <csw980928@gmail.com>
2024-08-04 20:44:05 +0900

    feat: 상품 수정 api 기능 구현(임시)

--- src/main/java/com/samsam/travel/travelcommerce/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/ticket/repository/TicketRepository.java
@@ -2,6 +2,26 @@ package com.samsam.travel.travelcommerce.domain.ticket.repository;
 
 import com.samsam.travel.travelcommerce.dao.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(
+            "UPDATE Ticket t " +
+            "SET " +
+                "t.title        =   :#{#ticket.title}, " +
+                "t.contents     =   :#{#ticket.contents}, " +
+                "t.place        =   :#{#ticket.place}, " +
+                "t.price        =   :#{#ticket.price}, " +
+                "t.startDate    =   :#{#ticket.startDate}, " +
+                "t.endDate      =   :#{#ticket.endDate} " +
+            "WHERE t.ticketId   =   :#{#ticket.ticketId} " +
+            "AND t.user.userId  =   :#{#ticket.user.userId}"
+    )
+    int updateTicket(@Param("ticket") Ticket ticket);
 }
