35be4f15fd840a65fdb5634a2851c4e2d87a782f
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-05 17:31:10 +0900

    feat: 회원 가입 시 중복 에러 추가(#5)

--- src/main/java/com/samsam/travel/travelcommerce/domain/user/service/UserAuthService.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/service/UserAuthService.java
@@ -1,10 +1,6 @@
 package com.samsam.travel.travelcommerce.domain.user.service;
 
-<<<<<<< HEAD
 import com.samsam.travel.travelcommerce.global.error.exception.UserDuplicateException;
-=======
-import com.samsam.travel.travelcommerce.domain.user.exception.UserDuplicateException;
->>>>>>> f7c903b (feat: 유저 회원 가입 기능 구현(#5))
 import com.samsam.travel.travelcommerce.domain.user.repository.UserRepository;
 import com.samsam.travel.travelcommerce.dto.user.SignUpRequest;
 import com.samsam.travel.travelcommerce.entity.User;
