82008d7d0e9976dc225a87e828d60d3518ce9d01
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-05 17:41:08 +0900

    feat: 유저 로그인 기능 구현 (#17)

--- src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AuthController.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AuthController.java
@@ -1,6 +1,8 @@
 package com.samsam.travel.travelcommerce.domain.user.api;
 
 import com.samsam.travel.travelcommerce.domain.user.service.UserAuthService;
+import com.samsam.travel.travelcommerce.dto.user.LoginRequest;
+import com.samsam.travel.travelcommerce.dto.user.LoginResponse;
 import com.samsam.travel.travelcommerce.dto.user.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 로그인, 회원가입 등 인증 관련 API를 처리하는 컨트롤러입니다.
  *
- * @author 당신의 이름
+ * @author lavin
  * @since 1.0
  */
 @RestController
@@ -24,6 +26,18 @@ public class AuthController {
      * 사용자 인증 서비스
      */
     private final UserAuthService userAuthService;
+
+    /**
+     * 로그인 API
+     *
+     * @param loginRequest 로그인 요청 데이터
+     * @return 로그인 성공 여부와 로그인 응답 데이터
+     */
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        LoginResponse result = userAuthService.login(loginRequest);
+        return ResponseEntity.ok(result);
+    }
 
     /**
      * 회원 가입 API
--- src/main/java/com/samsam/travel/travelcommerce/domain/user/service/UserAuthService.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/service/UserAuthService.java
@@ -1,16 +1,23 @@
 package com.samsam.travel.travelcommerce.domain.user.service;
 
-import com.samsam.travel.travelcommerce.global.error.exception.UserDuplicateException;
 import com.samsam.travel.travelcommerce.domain.user.repository.UserRepository;
+import com.samsam.travel.travelcommerce.dto.user.LoginRequest;
+import com.samsam.travel.travelcommerce.dto.user.LoginResponse;
 import com.samsam.travel.travelcommerce.dto.user.SignUpRequest;
 import com.samsam.travel.travelcommerce.entity.User;
 import com.samsam.travel.travelcommerce.entity.model.Role;
+import com.samsam.travel.travelcommerce.global.error.exception.UserDuplicateException;
 import com.samsam.travel.travelcommerce.global.status.ErrorCode;
 import com.samsam.travel.travelcommerce.security.auth.jwt.CustomPasswordAuthenticationToken;
 import com.samsam.travel.travelcommerce.security.auth.jwt.JwtAuthToken;
 import com.samsam.travel.travelcommerce.security.auth.jwt.JwtAuthTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,7 +28,7 @@ import java.util.Optional;
  * 이 클래스는 사용자 인증 및 권한 부여 서비스를 제공합니다.
  * 로그인 및 회원 가입을 위한 메서드를 포함합니다.
  *
- * @author 당신의 이름
+ * @author lavin
  * @since 1.0
  */
 @Service
@@ -61,6 +68,30 @@ public class UserAuthService {
                 .registDate(LocalDateTime.now())
                 .build();
         userRepository.save(newUser);
+    }
+
+    /**
+     * 성공적인 로그인을 위해 사용자를 인증하고 JWT 토큰을 생성합니다.
+     *
+     * @param dto 사용자의 아이디와 비밀번호가 포함된 로그인 요청입니다.
+     * @return JWT 토큰과 사용자 역할을 포함하는 LoginResponse 개체입니다.
+     * @throws AuthenticationException If the authentication fails.
+     */
+    public LoginResponse login(LoginRequest dto) throws AuthenticationException {
+        CustomPasswordAuthenticationToken token = new CustomPasswordAuthenticationToken(
+                dto.getId(), dto.getPassword()
+        );
+
+        Authentication authentication = authenticationManager.authenticate(token);
+
+        String role = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findAny().orElseThrow(()
+                        -> new BadCredentialsException("Role information is missing."));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwtToken = createToken((CustomPasswordAuthenticationToken) authentication);
+        return new LoginResponse(jwtToken, role);
     }
 
     /**
