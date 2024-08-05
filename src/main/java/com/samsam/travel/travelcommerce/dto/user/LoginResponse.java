27f82d985b364b73dc3e7a71fbdbaed811bf767a
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-05 17:38:46 +0900

    feat:사용자 로그인 관련 dto 생성(#17)

---
+++ src/main/java/com/samsam/travel/travelcommerce/dto/user/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.samsam.travel.travelcommerce.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 로그인 반환 dto
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String role;
+}
