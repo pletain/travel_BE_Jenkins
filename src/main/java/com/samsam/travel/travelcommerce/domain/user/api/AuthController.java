33fc7f5972d1e92835b0ed5a9b0661ee6f249153
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-06 10:18:37 +0900

    feat: ApiResponse 도입

--- src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AuthController.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AuthController.java
@@ -2,6 +2,7 @@ package com.samsam.travel.travelcommerce.domain.user.api;
 
 import com.samsam.travel.travelcommerce.domain.user.service.UserAuthService;
 import com.samsam.travel.travelcommerce.dto.user.SignUpRequest;
+import com.samsam.travel.travelcommerce.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.samsam.travel.travelcommerce.global.status.CommonCode.SUCCESS_SIGN_UP;
 
 /**
  * 로그인, 회원가입 등 인증 관련 API를 처리하는 컨트롤러입니다.
  *
- * @author 당신의 이름
+ * @author lavin
  * @since 1.0
  */
 @RestController
@@ -26,14 +28,20 @@ public class AuthController {
     private final UserAuthService userAuthService;
 
     /**
-     * 회원 가입 API
+     * 회원가입 API 요청을 처리합니다.
      *
-     * @param signUpRequest 회원 가입 요청 데이터
-     * @return 성공 여부
+     * 이 메서드는 {@link UserAuthService#signUp(SignUpRequest)} 메서드를 호출하여 회원가입 요청을 처리합니다.
+     * 그런 다음, 제공된 {@link SignUpRequest#getId()}와 {@link com.samsam.travel.travelcommerce.global.status.CommonCode#SUCCESS_SIGN_UP} 상수를 사용하여 성공 응답 메시지를 구성합니다.
+     * 마지막으로, 적절한 HTTP 상태 코드와 구성된 응답을 포함하는 ResponseEntity를 반환합니다.
+     *
+     * @param signUpRequest 회원가입 요청 데이터
+     * @return API 응답을 포함하는 ResponseEntity. HTTP 상태 코드와 구성된 응답이 있습니다.
      */
     @PostMapping("/signUp")
-    public ResponseEntity<String> signUp(@RequestBody SignUpRequest signUpRequest) {
+    public ResponseEntity<ApiResponse> signUp(@RequestBody SignUpRequest signUpRequest) {
         userAuthService.signUp(signUpRequest);
-        return ResponseEntity.ok("SUCCESS");
+        String formattedMessage = String.format(SUCCESS_SIGN_UP.getMessage(), signUpRequest.getId());
+        ApiResponse response = ApiResponse.createResponse(SUCCESS_SIGN_UP, formattedMessage);
+        return ResponseEntity.status(SUCCESS_SIGN_UP.getHttpStatus()).body(response);
     }
 }
