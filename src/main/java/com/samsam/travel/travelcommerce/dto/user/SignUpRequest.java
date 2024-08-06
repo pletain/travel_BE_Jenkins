0acb2ac8303d2be403adca88a854fd96a1aa4ab0
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-06 11:24:25 +0900

    feat: 회원가입 테스트 코드 추가 - 고유한 사용자 ID로 회원가입 시 새로운 사용자 저장 테스트 - 존재하는 사용자 ID로 회원가입 시 중복 사용자 예외 발생 테스트

--- src/main/java/com/samsam/travel/travelcommerce/dto/user/SignUpRequest.java
+++ src/main/java/com/samsam/travel/travelcommerce/dto/user/SignUpRequest.java
@@ -1,12 +1,14 @@
 package com.samsam.travel.travelcommerce.dto.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 /**
  * 회원 가입 요청 dto
  */
 @Getter
+@Builder
 @AllArgsConstructor
 public class SignUpRequest {
     private String id;
