853a85dfb6bdd0d92521e8c570baf0df066e17b9
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-07 06:14:54 +0900

    feat: User 엔티티를 UserInfoResponse로 변환 하는 메소드 추가(#18)

--- src/main/java/com/samsam/travel/travelcommerce/entity/User.java
+++ src/main/java/com/samsam/travel/travelcommerce/entity/User.java
@@ -1,5 +1,6 @@
 package com.samsam.travel.travelcommerce.entity;
 
+import com.samsam.travel.travelcommerce.dto.user.UserInfoResponse;
 import com.samsam.travel.travelcommerce.entity.model.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -57,4 +58,18 @@ public class User {
         return Objects.hash(userId);
     }
 
+    /**
+     * 현재 User 엔티티를 UserInfoResponse 객체로 변환합니다.
+     * 이 메서드는 외부 시스템이나 클라이언트에게 User 엔티티의 간단한 표현을 제공하기 위해 사용됩니다.
+     *
+     * @return UserInfoResponse 객체.
+     */
+    public UserInfoResponse toUserInfoResponse() {
+        return UserInfoResponse.builder()
+                .id(this.userId)
+                .name(this.name)
+                .phone(this.phone)
+                .role(this.role)
+                .build();
+    }
 }
