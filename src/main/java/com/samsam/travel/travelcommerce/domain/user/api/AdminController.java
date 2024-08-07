2077b322935567041d504f6a3c45f63fd76883c1
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-07 21:56:56 +0900

    fix: assignRoleToUser 중복 메소드 제거

--- src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AdminController.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AdminController.java
@@ -56,21 +56,4 @@ public class AdminController {
         String formattedMessage = String.format(SUCCESS_ASSIGN_ROLE.getMessage(), userId, newRole);
         return ResponseUtil.createApiResponse(SUCCESS_ASSIGN_ROLE, formattedMessage);
     }
-
-    /**
- * 지정된 사용자에게 새로운 역할을 할당합니다.
- *
- * @param userDetails 인증된 관리자의 세부 정보. Spring Security 컨텍스트에서 가져옵니다.
- * @param userId       역할을 할당할 사용자 ID.
- * @param newRole      새로운 역할.
- * @return ResponseEntity<ApiResponse>에 ApiResponse를 포함하고 있는 ResponseEntity.
- *         ApiResponse에는 상태 코드 200(OK)이 있으며, data 필드에 성공 메시지가 있습니다.
- */
-@PutMapping("/users/{userId}/role")
-public ResponseEntity<ApiResponse> assignRoleToUser(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String userId, @RequestParam String newRole) {
-    adminService.updateUserRole(userId, newRole, userDetails.getUsername());
-    String formattedMessage = String.format(SUCCESS_ASSIGN_ROLE.getMessage(), userId, newRole);
-    ApiResponse response = ApiResponse.createResponse(SUCCESS_ASSIGN_ROLE, formattedMessage);
-    return ResponseEntity.status(SUCCESS_ASSIGN_ROLE.getHttpStatus()).body(response);
-}
 }
