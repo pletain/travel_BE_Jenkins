f5b1bf34d19bbfebaa8bad0424b613dff259858e
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-07 12:57:38 +0900

    feat: Repository에서 사용자 ID로 역할을 찾아 반환하는 메소드 추가

--- src/main/java/com/samsam/travel/travelcommerce/domain/user/repository/UserRepository.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/repository/UserRepository.java
@@ -1,7 +1,18 @@
 package com.samsam.travel.travelcommerce.domain.user.repository;
 
 import com.samsam.travel.travelcommerce.entity.User;
+import com.samsam.travel.travelcommerce.entity.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    /**
+     * 사용자 ID로 역할을 찾아 반환합니다.
+     *
+     * @param userId 사용자 ID.
+     * @return 사용자 ID에 해당하는 역할.
+     */
+    @Query("select u.role from User u where u.userId = ?1")
+    Role findRoleByUserId(String userId);
 }
