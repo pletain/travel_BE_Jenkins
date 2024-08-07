4a5a064eb639cb5eaa3ea82d336bc2a0c1b6ff86
KIM TAE HYEONG <thk12378905@gmail.com>
2024-08-07 21:18:19 +0900

    refactor: assignRoleToUser 메소드에 ResponseUtil 반영

--- src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AdminController.java
+++ src/main/java/com/samsam/travel/travelcommerce/domain/user/api/AdminController.java
@@ -16,8 +16,9 @@ import static com.samsam.travel.travelcommerce.global.status.CommonCode.SUCCESS_
 import static com.samsam.travel.travelcommerce.global.status.CommonCode.SUCCESS_ASSIGN_ROLE;
 
 /**
- * 이 컨트롤러는 관리자 관련 API를 처리하는 컨트롤러입니다.
+ * 관리자 관련 API 요청을 처리하는 컨트롤러입니다.
  *
+ * @author lavin
  * @since 1.0
  */
 @RestController
@@ -41,19 +42,18 @@ public class AdminController {
     }
 
     /**
- * 지정된 사용자에게 새로운 역할을 할당합니다.
- *
- * @param userDetails 인증된 관리자의 세부 정보. Spring Security 컨텍스트에서 가져옵니다.
- * @param userId       역할을 할당할 사용자 ID.
- * @param newRole      새로운 역할.
- * @return ResponseEntity<ApiResponse>에 ApiResponse를 포함하고 있는 ResponseEntity.
- *         ApiResponse에는 상태 코드 200(OK)이 있으며, data 필드에 성공 메시지가 있습니다.
- */
-@PutMapping("/users/{userId}/role")
-public ResponseEntity<ApiResponse> assignRoleToUser(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String userId, @RequestParam String newRole) {
-    adminService.updateUserRole(userId, newRole, userDetails.getUsername());
-    String formattedMessage = String.format(SUCCESS_ASSIGN_ROLE.getMessage(), userId, newRole);
-    ApiResponse response = ApiResponse.createResponse(SUCCESS_ASSIGN_ROLE, formattedMessage);
-    return ResponseEntity.status(SUCCESS_ASSIGN_ROLE.getHttpStatus()).body(response);
-}
+     * 지정된 사용자에게 새로운 역할을 할당합니다.
+     *
+     * @param userDetails 인증된 관리자의 세부 정보. Spring Security 컨텍스트에서 가져옵니다.
+     * @param userId       역할을 할당할 사용자 ID.
+     * @param newRole      새로운 역할.
+     * @return ResponseEntity<ApiResponse>에 ApiResponse를 포함하고 있는 ResponseEntity. ApiResponse에는 상태 코드 200(OK)이 있으며, data 필드에 성공 메시지가 있습니다.
+     * @since 1.0
+     */
+    @PutMapping("/users/{userId}/role")
+    public ResponseEntity<ApiResponse> assignRoleToUser(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String userId, @RequestParam String newRole) {
+        adminService.updateUserRole(userId, newRole, userDetails.getUsername());
+        String formattedMessage = String.format(SUCCESS_ASSIGN_ROLE.getMessage(), userId, newRole);
+        return ResponseUtil.createApiResponse(SUCCESS_ASSIGN_ROLE, formattedMessage);
+    }
 }
